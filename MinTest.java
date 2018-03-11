adc60e9273408c5de59ed6730799c72544df71a8
Surafel Alamneh <suramiheret@gmail.com>
2018-03-11 16:28:15 +0100

    TestMin

--- MinTest.java
+++ MinTest.java
@@ -5,7 +5,7 @@ public class MinTest {
 			int[] a = {5,1,7};
 			int res = Min.find_min(a);
 			if(res == 1){
-				System.out.print("Test successful\n");
+				System.out.print(" Final testing please just work\n");
 			}
 		} 
 
